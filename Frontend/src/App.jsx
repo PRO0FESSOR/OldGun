85a8676d94039f9c6d4ed7ea25e388eedb85257e
PRO0FESSOR <siddharth96549@gmail.com>
2024-09-02 17:20:18 +0530

    homepage ,waiting area , playground component added and routing added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom'
+import Playground from './components/Playground'
+import Waiting from './components/Waiting'
+import Homepage from './components/Homepage'
 
 const App = () => {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-    app
-    </h1>
+  <>
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/waiting" element={<Waiting/>} />
+        <Route path="/playground" element={<Playground/>} />
+      </Routes>
+      </Router>
+     </div>    
   </>
   )
 }
